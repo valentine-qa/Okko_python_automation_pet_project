fbf68edda0832e987a2e17747418778b85ca4e25
valentine-qa <valentine.borodich13@gmail.com>
2025-06-23 09:56:24 +0300

    Changed config

--- conftest.py
+++ conftest.py
@@ -33,7 +33,6 @@ def setup_browser(request):
         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-
     browser = Browser(Config(driver))
     yield browser
 
